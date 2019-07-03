9424a7fe71f8b262a9102b36a59571cc08a8158e
semux <admin@semux.org>
2019-07-03 21:22:02 +0100

    Add build script for macos

---
+++ macos/build.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+JDK_URL=https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_osx-x64_bin.tar.gz
+JDK_FILE=openjdk-11.0.2_osx-x64_bin.tar.gz
+JDK_DIR=jdk-11.0.2.jdk
+JVM_DIR=jvm
+
+# change work directory
+cd "$(dirname "$0")/../"
+
+if [ -f "$JDK_FILE" ]
+then
+	echo "Using cached $JDK_FILE"
+else
+	echo "Downloading openjdk from $JDK_URL"
+	wget $JDK_URL || exit 1
+fi
+
+echo "Unzipping openjdk"
+tar -xvf $JDK_FILE
+
+echo "Building Java Runtime"
+./jdk-11.0.2.jdk/Contents/Home/bin/jlink \
+--output $JVM_DIR \
+--compress 2 \
+--no-man-pages \
+--add-modules java.base,java.desktop,java.management,java.logging,jdk.unsupported \
+|| exit 2
+
+echo "Creating an archive"
+tar -cvzf jvm-11-macos.tar.gz jvm/
+
+echo "Cleaning up"
+rm -fr $JDK_DIR
+rm -fr $JVM_DIR
